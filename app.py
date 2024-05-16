fff83239bc48f2ef98c1b6dfb32910aed3882b8d
luhenrique06 <luhenrique06@gmail.com>
2024-05-16 09:54:18 -0300

    update

--- app.py
+++ app.py
@@ -27,21 +27,6 @@ def search_users():
     })
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
-    if file and allowed_file(file.filename):
-        img_bytes = file.read()
-        hash_md5 = md5(img_bytes).hexdigest()
-        return jsonify({"md5": hash_md5})
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
